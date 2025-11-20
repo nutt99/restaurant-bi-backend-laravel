9856852f286a028b5b1a2fc7b64d14bfaed70133
nutt99 <NataGaming766@gmail.com>
2025-11-21 00:54:42 +0700

    fixing some shit

--- routes/api.php
+++ routes/api.php
@@ -15,25 +15,34 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 
-    Route::apiResource('menus', MenuController::class);
+    //owner access
+    Route::middleware('role:OWNER')->group(function(){
 
-    Route::prefix('dashboard')->group(function () {
-        Route::get('/daily', [DashboardController::class, 'daily']);
-        Route::get('/weekly', [DashboardController::class, 'weekly']);
-        Route::get('/monthly', [DashboardController::class, 'monthly']);
-        Route::get('/summary', [DashboardController::class, 'summary']);
-        Route::get('/top-menu', [DashboardController::class, 'topMenu']);
-        Route::get('/trend', [DashboardController::class, 'trend']);
-        Route::get('/growth', [DashboardController::class, 'growth']);
-        Route::get('/best-sales-day', [DashboardController::class, 'bestSalesDay']);
+        Route::prefix('dashboard')->group(function () {
+            Route::get('/daily', [DashboardController::class, 'daily']);
+            Route::get('/weekly', [DashboardController::class, 'weekly']);
+            Route::get('/monthly', [DashboardController::class, 'monthly']);
+            Route::get('/summary', [DashboardController::class, 'summary']);
+            Route::get('/top-menu', [DashboardController::class, 'topMenu']);
+            Route::get('/trend', [DashboardController::class, 'trend']);
+            Route::get('/growth', [DashboardController::class, 'growth']);
+            Route::get('/best-sales-day', [DashboardController::class, 'bestSalesDay']);
+        });
+
+        Route::get('/report', [ReportController::class, 'index']);
+        Route::get('/report/pdf', [ReportController::class, 'exportPdf']);
+        Route::get('/report/excel', [ReportController::class, 'exportExcel']);
     });
 
-    Route::get('/transactions', [TransactionController::class, 'index']);
-    Route::get('/transactions/{id}', [TransactionController::class, 'show']);
-    Route::post('/transactions', [TransactionController::class, 'store']);
-    Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
+    Route::middleware('role:OWNER,CASHIER')->group(function(){
 
-    Route::get('/reports', [ReportController::class, 'index']);
-    Route::get('/reports/pdf', [ReportController::class, 'exportPdf']);
-    Route::get('/reports/excel', [ReportController::class, 'exportExcel']);
+        Route::apiResource('menu', MenuController::class);
+
+        Route::get('/transactions', [TransactionController::class, 'index']);
+        Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+        Route::post('/transactions', [TransactionController::class, 'store']);
+        Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
+    });
+
+    
 });
